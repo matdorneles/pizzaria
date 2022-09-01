537582baed9479bba4145df452838171416d34a8
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-08-31 22:22:17 -0300

    finished register service

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,4 +1,5 @@
 import prismaClient from "../../prisma";
+import { hash } from "bcryptjs";
 
 interface UserRequest {
   name: string;
@@ -24,12 +25,19 @@ class CreateUserService {
       throw new Error("Email já está registrado");
     };
 
+    const passwordHash = await hash(password, 8);
+
     const user = await prismaClient.user.create({
       data: {
         name: name,
         email: email,
-        password: password,
-      }
+        password: passwordHash,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
     });
 
     return user;
