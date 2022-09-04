397a2935bfdedb463fd00f9080e4fb420d8ef268
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-09-04 18:37:09 -0300

    added JWT into authentication system

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthRequest {
   email: string;
@@ -26,7 +27,25 @@ class AuthUserService {
       throw new Error("Incorrect user/password");
     }
 
-    return { ok: true };
+    // If all OK, generate user token
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "30d"
+      }
+    );
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token
+    };
   };    
 };
 
