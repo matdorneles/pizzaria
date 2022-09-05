69782e4915cfdca6e9d6f5906e34571e9f9c19bf
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-09-05 20:41:27 -0300

    Created authentication middleware

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
 // USER ROUTES
 router.post("/users", new CreateUserController().handle);
-
-// LOGIN ROUTES
 router.post("/session", new AuthUserController().handle);
+router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 export { router };
