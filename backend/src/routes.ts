effbb173d9b0e75e13da321a77d8b3d17190191e
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-09-07 16:59:56 -0300

    Remove Item code

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -16,6 +16,7 @@ import { ListByCategoryController } from "./controllers/product/ListByCategoryCo
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 import { AddItemController } from "./controllers/order/AddItemController";
+import { RemoveItemController } from "./controllers/order/RemoveItemController";
 
 const router = Router();
 const upload = multer(uploadConfig.upload("./tmp"));
@@ -37,5 +38,6 @@ router.get("/category/product", isAuthenticated, new ListByCategoryController().
 router.post("/order", isAuthenticated, new CreateOrderController().handle);
 router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
 router.post("/order/item", isAuthenticated, new AddItemController().handle);
+router.delete("/order/item", isAuthenticated, new RemoveItemController().handle);
 
 export { router };
