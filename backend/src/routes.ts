b868fdc83b0dec0a79af35b2d121d2a7d6d97e27
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-09-06 22:34:10 -0300

    create category code

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 const router = Router();
 
@@ -10,5 +11,8 @@ const router = Router();
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+// CATEGORY ROUTES
+router.post("/category", isAuthenticated, new CreateCategoryController().handle);
 
 export { router };
