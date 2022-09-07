e5c3b04bb496945d6b449766d3adc371dca24181
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-09-07 16:10:38 -0300

    create new order code

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -13,6 +13,8 @@ import { ListCategoryController } from "./controllers/category/ListCategoryContr
 import { CreateProductController } from "./controllers/product/CreateProductController";
 import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 
+import { CreateOrderController } from "./controllers/order/CreateOrderController";
+
 const router = Router();
 const upload = multer(uploadConfig.upload("./tmp"));
 
@@ -29,5 +31,7 @@ router.get("/category", isAuthenticated, new ListCategoryController().handle);
 router.post("/product", isAuthenticated, upload.single("file"), new CreateProductController().handle);
 router.get("/category/product", isAuthenticated, new ListByCategoryController().handle);
 
+// ORDER ROUTES
+router.post("/order", isAuthenticated, new CreateOrderController().handle);
 
 export { router };
