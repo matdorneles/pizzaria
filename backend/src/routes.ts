ec6a99f7248926b9104902a73efaefc4034b28e0
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-09-06 23:05:49 -0300

    Create product code

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,6 +5,7 @@ import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
 
 const router = Router();
 
@@ -16,5 +17,8 @@ router.get("/me", isAuthenticated, new DetailUserController().handle);
 // CATEGORY ROUTES
 router.post("/category", isAuthenticated, new CreateCategoryController().handle);
 router.get("/category", isAuthenticated, new ListCategoryController().handle);
+
+// PRODUCT ROUTES
+router.post("/product", isAuthenticated, new CreateProductController().handle);
 
 export { router };
