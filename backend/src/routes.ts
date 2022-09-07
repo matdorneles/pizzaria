67206e94ad046fb26e079a1858d5c315aa5c76eb
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-09-07 18:45:26 -0300

    Added option to finish and send order

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -17,6 +17,7 @@ import { CreateOrderController } from "./controllers/order/CreateOrderController
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
+import { SendOrderController } from "./controllers/order/SendOrderController";
 
 const router = Router();
 const upload = multer(uploadConfig.upload("./tmp"));
@@ -39,5 +40,6 @@ router.post("/order", isAuthenticated, new CreateOrderController().handle);
 router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
 router.post("/order/item", isAuthenticated, new AddItemController().handle);
 router.delete("/order/item", isAuthenticated, new RemoveItemController().handle);
+router.put("/order/send", isAuthenticated, new SendOrderController().handle);
 
 export { router };
