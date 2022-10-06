5c27b1a4f1ca0f25948527d2540ae73481c51f48
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-10-06 19:49:11 -0300

    starting frontend

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/home.module.scss";
+
+import logoImg from "../../public/logo.svg";
+
+import { Input } from "../components/ui/Input"
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Pizzaria - Faça seu login</title>
+    </Head>
+    <div className={styles.containerCenter}>
+      <Image src={logoImg} alt="Logo Pizzaria" />
+
+      <div className={styles.login}>
+        <form>
+          <Input 
+          placeholder="Digite seu email" 
+          type="text"
+          />
+
+          <Input 
+          placeholder="Digite sua senha"
+          type="password" 
+          />
+        </form>
+      </div>
+    </div>
+    </>
+  )
+}
