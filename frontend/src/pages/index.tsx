21ff6267c411f0fa3cdd78be4cce28176846cbd1
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-10-13 21:23:06 -0300

    finished login page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,6 +5,7 @@ import styles from "../../styles/home.module.scss";
 import logoImg from "../../public/logo.svg";
 
 import { Input } from "../components/ui/Input"
+import { Button } from "../components/ui/Button";
 
 export default function Home() {
   return (
@@ -17,16 +18,16 @@ export default function Home() {
 
       <div className={styles.login}>
         <form>
-          <Input 
-          placeholder="Digite seu email" 
-          type="text"
-          />
+          <Input placeholder="Digite seu email" type="text" />
 
-          <Input 
-          placeholder="Digite sua senha"
-          type="password" 
-          />
+          <Input placeholder="Digite sua senha" type="password" />
+
+          <Button type="submit" loading={false} >
+            Acessar
+          </Button>
         </form>
+
+        <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
       </div>
     </div>
     </>
